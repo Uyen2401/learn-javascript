c048e9c391b04f47c8f7dfbe785c4523a1bba860
uyen-nguyenthuy-tpv-clv <uyen.nguyenthuy.tpv@one-line.com>
2024-12-08 16:25:24 +0700

    comment in javascript

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,5 +1,11 @@
+// Khai bao bien
 var fullName = 'Nguyen Thuy Uyen';
-var age = 18;
+var age = 18; // Khai bao tuoi
 
+/**
+ * Comment in JavaScript
+ */
+
+// Goi ham thong bao
 alert(fullName);
 alert(age);
