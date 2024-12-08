b24a6073df0e9cdb32de2fd1f1f9634affd9cf9b
uyen-nguyenthuy-tpv-clv <uyen.nguyenthuy.tpv@one-line.com>
2024-12-08 16:38:51 +0700

    Function Build-in JavaScript

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,5 +1,5 @@
 // Khai bao bien
-var fullName = 'Nguyen Thuy Uyen';
+var fullName = "Nguyen Thuy Uyen";
 var age = 18; // Khai bao tuoi
 
 /**
@@ -7,5 +7,29 @@ var age = 18; // Khai bao tuoi
  */
 
 // Goi ham thong bao
-alert(fullName);
-alert(age);
+// alert(fullName);
+// alert(age);
+//
+/**
+ * Ham Build-in JavaScript
+ * 1. Alert
+ * 2.Console
+ * 3.Confirm
+ * 4.Prompt
+ * Set timeout
+ * Set interval
+ */
+// 2. Console
+console.log(fullName);
+// 3.Confirm
+// confirm("Xac nhan ban du tuoi!");
+// 4.Prompt
+// prompt("Xac nhan ban du tuoi!");
+// 5. Set timeout
+setTimeout(() => {
+  console.log('Thong bao ne')
+}, 1000);
+// 6.Set interval
+setInterval(()=>{
+    console.log('Log::::' + Math.random())
+},10)
