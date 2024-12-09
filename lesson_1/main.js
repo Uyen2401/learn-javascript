d9e9958d84115e4135e246951b1b564c2d4e4195
uyen-nguyenthuy-tpv-clv <uyen.nguyenthuy.tpv@one-line.com>
2024-12-09 07:19:50 +0700

    arithmetic/assignment/comparison/logical operator

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,6 +1,6 @@
 // Khai bao bien
 var fullName = "Nguyen Thuy Uyen";
-var age = 18; // Khai bao tuoi
+// var age = 18; // Khai bao tuoi
 
 /**
  * Comment in JavaScript
@@ -22,14 +22,43 @@ var age = 18; // Khai bao tuoi
 // 2. Console
 console.log(fullName);
 // 3.Confirm
-// confirm("Xac nhan ban du tuoi!");
+confirm("Xac nhan ban du tuoi!");
 // 4.Prompt
-// prompt("Xac nhan ban du tuoi!");
+prompt("Xac nhan ban du tuoi!");
 // 5. Set timeout
 setTimeout(() => {
-  console.log('Thong bao ne')
+  console.log("Thong bao ne");
 }, 1000);
 // 6.Set interval
 setInterval(()=>{
     console.log('Log::::' + Math.random())
 },10)
+
+/**
+ 1. Arithmetic: Toan tu so hoc
+ 2. Assignment: Toan tu gan
+ 3. Comparison: Toan tu so sánh
+ 4. Logical: Toan tu logic
+ */
+
+//  1.Arithmetic
+let Arithmetic = 1 + 2;
+console.log(Arithmetic);
+
+// 2.Assignment
+let firstName = "Nguyen"; // "="
+
+// 3. Comparison
+let age = 18;
+let height = 160;
+
+if( age < height){
+    console.log('Dung')
+}
+
+// 4.Logical
+
+if (age > 0 && height > 0){
+    console.log('age & height lớn hơn 0')
+}
+
