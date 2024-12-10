71d5e8b8dad0a864f79fc73a6653a7924e570dba
uyen-nguyenthuy-tpv-clv <uyen.nguyenthuy.tpv@one-line.com>
2024-12-10 07:48:20 +0700

    data type

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -231,3 +231,78 @@ if (!(rangeFrom > rangTo)) {
 } else {
     console.log('Invalid range')
 }
+
+
+// LESSON 13
+/**
+ 1. Primitive Data
+ - Number
+ - String
+ - Boolean
+ - Undefined
+ - Null
+ - Symbol 
+ 2.Complex Data
+ - Function
+ - Object : Array, Object
+ */
+
+//  Number type
+
+let rank = 100;
+let rate = 20.5;
+
+//  String type
+
+let string = 'Uyen Nguyen\'s' // == "Uyen Nguyen's" 
+console.log(string);
+
+// Boolean type
+
+let isFalse = false;
+
+// Undefined
+
+let cat;
+
+// Null
+
+let isNull = null; //nothing
+
+// Symbol
+
+let id = Symbol ('id'); // unique
+let id1 = Symbol ('id') // unique
+console.log(id === id1)
+
+// Function
+let myFunction = function(){
+    console.log('Hi. Xin chao cac ban') 
+}
+
+myFunction();
+
+// Object types
+// Object
+ let myObject = {
+    name: 'Uyen Nguyen', // key : value ,
+    age: '18',
+    myFunction: function(){
+        //
+    }
+ }
+
+ console.log('myObject:::', myObject)
+//  Array
+let myArray = [
+    'Javascript',
+    'PHP',
+    'Ruby'
+];
+console.log('myArray:::', myArray);
+
+// typeof: get the type of any value.
+console.log(typeof myObject);
+console.log(typeof id);
+console.log(typeof rank);
+console.log(typeof isNull); // return 'Object' ?????
