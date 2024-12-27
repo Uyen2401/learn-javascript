1f35024ee20fb48a690b861ac8d1233666bfccee
Pham Manh <phammanh@172.16.66.66>
2024-12-27 18:28:50 +0700

    add loop exercise

---
+++ exercises/loop/exercise.js
@@ -0,0 +1,63 @@
+const productsCard = [
+  {
+    id: 1,
+    name: "Tech Product 1",
+    quantity: 96,
+    price: 121,
+    discountPrice: 96,
+    stock: 10,
+  },
+  {
+    id: 2,
+    name: "Tech Product 2",
+    quantity: 47,
+    price: 236,
+    discountPrice: 212,
+    stock: 20,
+  },
+  {
+    id: 3,
+    name: "Tech Product 3",
+    quantity: 66,
+    price: 456,
+    discountPrice: 364,
+    stock: 0,
+  },
+  {
+    id: 4,
+    name: "Tech Product 4",
+    quantity: 85,
+    price: 668,
+    discountPrice: 601,
+    stock: 5,
+  },
+  {
+    id: 5,
+    name: "Tech Product 5",
+    quantity: 30,
+    price: 663,
+    discountPrice: 563,
+    stock: 4,
+  },
+];
+
+
+/**
+ * Yêu cầu
+ * - Sử dụng cả tất cả những vòng lặp đã học để giải từng bài
+ */
+
+// Bài 1. Tính số lượng sản phẩm trong giỏ hàng
+
+
+// Bài 2. Tìm danh sách sản phẩm còn hàng 
+
+
+// Bài 3. Tính tổng tiền chưa giảm
+
+
+// Bài 4. Tính tổng tiền sau giảm
+
+
+// Bài 5. Tính discount của từng sản phẩm và in ra danh sách sản phẩm + discountPercent
+
