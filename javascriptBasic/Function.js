f7631217d395435e3ab3ec16bb8488fc4198c27c
uyen-nguyenthuy-tpv-clv <uyen.nguyenthuy.tpv@one-line.com>
2025-01-11 20:58:35 +0700

    callback function

--- javascriptBasic/Function.js
+++ javascriptBasic/Function.js
@@ -9,14 +9,14 @@ Mục đích:
 - Dễ dàng hơn để loại bỏ các lỗi
 */
 function functionName() {
-    // Code
+  // Code
 }
 
 function sayHello() {
-    console.log("Hello");
+  console.log("Hello");
 
 }
- 
+
 // Calling function
 sayHello(); // return: Hello
 
@@ -55,17 +55,122 @@ sayHello(); // return: Hello
  */
 // Adding Parameters to Functions
 function displaySum(num1, num2) {
-    let total = num1 + num2;
-    console.log(total);
+  let total = num1 + num2;
+  console.log(total);
 }
- 
+
 displaySum(6, 20); // return: 26
 displaySum(-5, 17); // return: 12
 
 function showFullname(firstName, lastName) {
-    console.log(firstName + " " + lastName);
+  console.log(firstName + " " + lastName);
 }
 
 showFullname("Uyen", "Nguyen"); // Return: Uyen Nguyen
 showFullname("Uyen"); // Return: Uyen undefined
 
+//callback() - Hàm được chuyền qua đối số khi gọi hàm khác
+/**
+ Là hàm
+ Truyền qua đối số
+ Được gọi lại (Trong hàm nhận đối số)
+ */
+function myFunction(param) {
+  if (typeof param === "function") {
+    param();
+  }
+}
+
+function myCallback(value) {
+  console.log("Value:", value);
+}
+
+myFunction(myCallback);
+
+Array.prototype.map2 = function (callback) {
+  //nhận một hàm callback làm tham số. callback được gọi trên từng phần tử của mảng trong vòng lặp.
+  let output = [],
+    arrayLength = this.length;
+  for (let i = 0; i < arrayLength; i++) {
+    let result = callback(this[i], i);
+    console.log("result: ", result);
+    output.push(result);
+  }
+  return output;
+};
+let courses = ["Javascript", "PHP", "Ruby"];
+let htmls = courses.map2(function (course, index) {
+  // console.log(index, course)
+  return `<h2>${course}</h2>`;
+});
+
+console.log(htmls.join(""));
+console.log("------------");
+
+// map(): cho kq giống ở trên
+htmls = courses.map(function (course) {
+  return `<h2>${course}</h2>`;
+});
+console.log(htmls.join(""));
+
+// Exercise làm tương tự cho: forEach(), every(), find(), filter(), reduce()
+// 1.forEach2()
+Array.prototype.forEach2 = function (callback) {
+  let arrayLength = this.length;
+  for (let i = 0; i < arrayLength; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+courses.forEach2(function (course, index) {
+  console.log(`${index}: ${course}`);
+});
+
+console.log("------------");
+courses.forEach(function (course, index) {
+  console.log(`${index}: ${course}`);
+});
+
+// 2.every2()
+Array.prototype.every2 = function (callback) {
+  let arrayLength = this.length;
+  for (let i = 0; i < arrayLength; i++) {
+    if (!callback(this[i], i, this)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let numbers = [2, 4, 6];
+let allEven = numbers.every2(function (num) {
+  return num % 2 === 0;
+});
+console.log(allEven); // return: true
+
+console.log("------------");
+
+allEven = numbers.every(function (num) {
+  return num % 2 === 0;
+});
+console.log(allEven);
+
+// 3. find2()
+Array.prototype.find2 = function (callback) {
+  let arrayLength = this.length;
+  for (let i = 0; i < arrayLength; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
+
+let listNumber = [1, 2, 3, 4, 5];
+let result = listNumber.find2((num) => num > 3);
+console.log("Số đầu tiên lớn hơn 3 là::::", result);
+
+console.log("------------");
+
+result = listNumber.find((num) => num > 3);
+console.log("Số đầu tiên lớn hơn 3 là::::", result);
